23f4e1d8097082843d4b5f525296e623bfc359ce
Sophia Ciocca <sophiaciocca@gmail.com>
2017-05-12 15:39:02 -0400

    updating with correct socket eventemitters

--- browser/app.js
+++ browser/app.js
@@ -2,21 +2,50 @@
 // var app = require('express')();
 // var server = require('http').Server(app);
 
-// //hook into whiteboard.draw and console.log payload
-//var socket = io.connect();
-
-whiteboard.on('draw', (start,end,strokeColor) => {
-    console.log('start',start,'end',end,'color',strokeColor);
-    
-});
-
+//create socket of browser that's connecting
 var socket = io(window.location.origin);
 
+//when the socket is connected, start doing everything else
 socket.on('connect', function() {
-    console.log('I have made a persistent two-way connection to the server!');
+    console.log("I am connected!");
 
-    socket.emit('sayHello', {msg : "shalom"})
+    //event listener - listen for when whiteboard draws
+    whiteboard.on('draw', (start, end, strokeColor) => {
+        socket.emit('aDraw', start, end, strokeColor);
+    });
+
+    //listen for other people drawing
+    socket.on('otherPersonDraw', function() {
+        console.log('Another person drew!');
+        //draw/render it on your screen
+        whiteboard.draw(start, end, color); //DON'T put 'true' as shouldBroadcast arg -- will create an infinite loop
+
+    })
+
 });
 
-socket.on()
+// Claire's and my beginnings of code (WRONG)
+// whiteboard.on('draw', (start,end,strokeColor) => {
+//     console.log('start',start,'end',end,'color',strokeColor);
+//     socket.emit('drawn',{'start':start})
+// });
+
+// socket.on('connect', function() {
+//     console.log('I have made a persistent two-way connection to the server!');
+
+//     socket.emit('sayHello', {msg : "shalom"})
+
+// });
+
+// socket.on()
+
+/*
+could have done better: explaining diff between SOCKET event emitters and OTHER event emitters.
+I was confused b/c from the getgo I thought we were using sockets to communicate for ALL event emitters / between front-end and back-end, etc.
+It took claire and I a while to even get started because we were so lost on what sockets even were, what exactly they were connecting, etc.
+
+Could explain that our front-end code is actually like the person VISITING the site, while our back-end code is the person SENDING the info? Is that even right?
+I was so confused on who I was, and what role I was supposed to be playing with any given code, what it was connecting to, etc.
+
+*/
 
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 /*
 1) How does the app know whether to use a typical HTTP request, or whether it needs to go to the socket?
+2) "io global"??? making it available everywhere? how did everything become available everywhere?
+3) Why did he have to BROADCAST the drawing when he got it, to render it on his screen? What does that mean?
+I thought it would be like 'render it on the screen'
 
+**4) I'm left rly confused about difference between server.js and app.js (front-end) at this point, in this workshop, and the relationship between those, vs the relationship between drawer and receiver.
 */
 
 var path = require('path');
 var express = require('express');
 var app = express(); // the app returned by express() is a JavaScript Function. Not something we can pass to our sockets!
+
 var socketio = require('socket.io');
 
 // app.listen() returns an http.Server object
@@ -14,25 +19,9 @@ var server = app.listen(1337, function () {
     console.log('The server is listening on port 1337!');
 });
 
+//takes our server and wraps it all in the io logic. so now, "io" is a socketserver (and an event emitter)
+//also, allows us to get client code of the socket automatically
 var io = socketio(server);
-
-//use socket serve as an event emitter to listen for new connections
-io.on('connection', function(socket) {
-    //receives newly connected socket, and will be called for EACH browser that connects to our server.
-    console.log('A new client has connected!');
-    console.log(socket.id);
-
-    socket.on('sayHello', function(msg) {
-        console.log('terminal ', msg)
-    })
-    
-    socket.on('disconnect', function() {
-        console.log(": ( disconnected");
-    });
-
-    
-})
-
 
 //static middleware to serve up all the JS in 'browser' folder
 app.use(express.static(path.join(__dirname, 'browser')));
@@ -41,3 +30,30 @@ app.use(express.static(path.join(__dirname, 'browser')));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+
+//use socket server as an event emitter to listen for new connections
+io.on('connection', function(socket) {
+    //receives newly connected socket, and will be called for EACH browser that connects to our server.
+    console.log('A new client has connected!');
+    console.log(socket.id);
+
+    socket.on('someInfo', function(payload) {
+        console.log('someInfo:', payload);
+        socket.emit('myInfo', 24);
+    })
+
+    //when you get a drawing, display it
+    socket.on('aDraw', (start, end, color) => {
+        //display/render the drawing - broadcast it to all the other connected sockets EXCEPT the drawer himself
+        socket.broadcast.emit('otherPersonDraw', start, end, color); //if we had done "io.sockets.emit", it would've gone out to everyone INCLUDING the drawer...
+
+
+    })
+    
+    socket.on('disconnect', function() {
+        console.log(": ( disconnected");
+    });
+
+    
+})
